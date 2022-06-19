97f24d3ee4677b6820e6170d6d0967b876061832
ArteyHub <92204258+ArteyHub@users.noreply.github.com>
2022-06-19 20:18:48 +0200

    New files and changes

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import os
 import requests
 import json
 import sys
+import configparser
+import settings
+
+
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -14,6 +18,7 @@ tFile = open('TOKEN.txt', 'r')
 TOKEN = tFile.read()
 client = commands.Bot(command_prefix='.')
 chList = {}
+role = settings.setup['mainRole']
 
 
 
@@ -23,17 +28,10 @@ async def on_ready():
 
 
 
-@client.command(pass_context=True)
-@commands.has_role('IMPERATOR')
-async def exit(ctx):
-    await ctx.reply('Shutting Down...')
-    sys.exit()
-
-
 @client.event
 async def on_voice_state_update(member, before, after): 
   
-    channel = client.get_channel(988055091640684639)    #988055091640684639
+    channel = client.get_channel(988055091640684639)
     channel = channel.voice_states
 
     for key in channel:
@@ -55,22 +53,41 @@ async def on_voice_state_update(member, before, after):
             
 
 
-
 @client.command(pass_context=True)
 async def changeName(ctx, message):
     author = ctx.author
-    
-    
+
     for channelAuth in list(chList):
         channelAuthName = int(chList[channelAuth])
         channelAuthName = client.get_channel(channelAuthName)
-       
-
+    
         if(str(author), author.voice.channel) == (channelAuth, channelAuthName):
             await author.voice.channel.edit(name= message)
-            await ctx.reply('Channel name has been changed to: ' + message, mention_author=False)
+            await ctx.reply('[+] Channel name has been changed to: ' + message, mention_author=False)
+
+
+
+@client.command(pass_context=True)
+@commands.has_role(role)
+async def botSetup(ctx):
+    guild = client.guilds[0]
+    for channel in settings.channels:
+        probeCategory = discord.utils.get(guild.categories, name=settings.channels[channel])
+        if probeCategory == None:
+            await guild.create_category(settings.channels[channel], overwrites=None, reason=None)
+        
+        probeChannels = discord.utils.get(guild.channels, name=channel)
+        if probeChannels == None:
+            await guild.create_text_channel(channel, category = settings.channels[channel], overwrites=None, reason=None)
+
+
+
+@client.command(pass_context=True)
+@commands.has_role(role)
+async def exit(ctx):
+    await ctx.reply('[+] Shutting Down...')
+    sys.exit()
+
 
 
 client.run(TOKEN)
-
-
---
+++ settings.py
@@ -0,0 +1,10 @@
+setup = {
+    'mainRole' : 'IMPERATOR', 
+    'complete' : False,
+}
+
+channels = {
+        'user-announcements' : 'd',
+        'court-channel' : 'd',
+        'bot-commands' : '▬▬bot-commands▬▬'
+        }
