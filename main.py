0f6fc93ccb838dbbda3bfdfdaa553cc3371d47a8
ArteyHub <92204258+ArteyHub@users.noreply.github.com>
2022-06-25 01:34:35 +0200

    FuckMeIHateMyLife

--- main.py
+++ main.py
@@ -70,13 +70,6 @@ async def on_voice_state_update(member, before, after):
           
 
 
-@client.command(pass_context=True)
-@commands.has_role(role)
-async def exit(ctx):
-    await ctx.reply('[+] Shutting Down...')
-    sys.exit()
-
-
 #Buttons--------------------------------------------------------------------------------------------
 class mButtons(nextcord.ui.View):
     def __init__(self, *, timeout=180):
@@ -91,49 +84,25 @@ class mButtons(nextcord.ui.View):
             if(str(author), author.voice.channel) == (channelAuth, channelAuthName):
                 await author.voice.channel.edit(name= message)
                 await ctx.reply('[+] Channel name has been changed to: ' + message, mention_author=False)
-        await interaction.response.send_message("Dipshit")
+        await interaction.response.send_message("select dis", view=self)
 
 
-
-    @nextcord.ui.button(label='Kanal oeffnen')
-    async def openCallback(self, button, interaction):
-        await interaction.response
-    
-    @nextcord.ui.button(label='Kanal schliessen')
-    async def closeCallback(self, button, interaction):
-        button.label = 'cytschka'
 
     @nextcord.ui.button(label='User kicken')
     async def kickCallback(self, button, interaction):
-        select = Select(
-        placeholder = "None",
-        options=[
-            nextcord.SelectOption(
-                label = '1',
-                description = 'dis description',
-                default= True
-            ),
-            nextcord.SelectOption(
-                label = '2',
-                description = 'dis description',
-                default= True
-            ),
-            nextcord.SelectOption(
-                label = '3',
-                description = 'dis description',
-                default= True
-            )])
-        view1 = self
-        view1.add_item(select)
-        await interaction.response.send_message("select dis", view=view1)
+        pass
 
-    @nextcord.ui.button(label='User Blocken')
-    async def blockCallback(self, button, interaction):
-        button.label = 'cytschka'
+
 
 @client.command()
 async def olla(ctx):
     view = mButtons()
-    await ctx.send("tylko", view=view)
+    await ctx.send("", view=mButtons())
+
+
+
+
+
+
 
 client.run(TOKEN)
